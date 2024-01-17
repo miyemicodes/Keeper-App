4fd7fb91953d8d474337f35b6e3326a647ba7035
miyemicodes <lizzymiyemi@gmail.com>
2024-01-16 20:36:12 -0800

    initial commit

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-function Note(){
+function Note(props){
   return(
     <div className="note">
-      <h1>First Entry</h1>
-      <p>I Love React JS ,and i hope our relationship isn't toxic</p>
+      <h1> {props.title} </h1>
+      <p> {props.content} </p>
     </div>
   );
 
